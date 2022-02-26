af72daa84467ae59f8c57764972c5794946f0a33
maayan24 <46651081+maayan24@users.noreply.github.com>
2022-02-26 21:58:58 +0200

    delete

---
+++ WebApi/WebApi/Controllers/officersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.DTO;
+using ClassLibrary.EF;
+namespace WebApi.Controllers
+{
+    public class officersController : ApiController
+    {
+        // GET api/<controller>
+        public List<OfficerDto> Get()
+        {
+            bgroup51_test2TraffixDBContext db = new bgroup51_test2TraffixDBContext();
+            return db.Officers.Select(x => new OfficerDto()
+            {
+                BadgeNum = x.BadgeNum,
+                Name = x.Name
+            }).ToList();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [Route("api/officer/{badgeNum}")]
+        public void Put(int badgeNum, [FromBody] string newName)
+        {
+            bgroup51_test2TraffixDBContext db = new bgroup51_test2TraffixDBContext();
+            Officer o = db.Officers.SingleOrDefault(x => x.BadgeNum == badgeNum);
+            if (o != null)
+            {
+                o.Name = newName;
+                db.SaveChanges();
+            }
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/WebApi/DTO/OfficerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.DTO
+{
+    public class OfficerDto
+    {
+        public int BadgeNum;
+        public string Name;
+    }
+}
